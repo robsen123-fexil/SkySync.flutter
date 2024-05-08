28920cf39c81718ee7e61653b9075b1bb38169ea
robsen123-fexil <robsenteshoma1@gmail.com>
2024-05-08 20:16:23 +0300

    actual location

--- skysync/lib/screens/loading.dart
+++ skysync/lib/screens/loading.dart
@@ -1,11 +1,15 @@
-// ignore_for_file: unused_import, unused_local_variable, avoid_print
+// ignore_for_file: unused_import, unused_local_variable, avoid_print, prefer_const_constructors, use_build_context_synchronously
+
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:skysync/function/location.dart';
 import 'package:http/http.dart';
 import 'package:skysync/function/weathermap.dart';
+import 'package:skysync/screens/resultpage.dart';
 
-const api = ' aaee36e539a3634b5a211876369ca3d1';
+const api = 'aaee36e539a3634b5a211876369ca3d1';
 
 class Loadingscreen extends StatefulWidget {
   const Loadingscreen({super.key});
@@ -23,26 +27,32 @@ class _LoadingscreenState extends State<Loadingscreen> {
     getlocationData();
   }
 
-  // void getWeatherdata() async {
-  //   Response response = await get(Uri.parse(
-  //       "https://api.openweathermap.org/data/2.5/weather?lat=33.8&lon=122.0&appid=aaee36e539a3634b5a211876369ca3d1"));
-  //   var jsondata = response.body;
-  //   print(jsondata);
-  // }
+ 
 
   void getlocationData() async {
     Networking networking = Networking(
         Url:
-            'https://api.openweathermap.org/data/2.5/weather?lat=33.8&lon=122.0&appid=aaee36e539a3634b5a211876369ca3d1');
+            'https://api.openweathermap.org/data/2.5/weather?lat=33.8&lon=122.0&appid=aaee36e539a3634b5a211876369ca3d1&units=metric');
     Location location = Location();
     var location1 = await location.getlocation();
     print(location1);
     var weatherdata = await networking.getweatherdata();
-    print(weatherdata);
+   
+    var data = jsonDecode(weatherdata)['main']['temp'];
+    print(data);
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => ResultPage(weatherdata: weatherdata)));
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        theme: ThemeData.dark(),
+        home: Scaffold(
+          body: SpinKitThreeBounce(
+            color: Colors.white,
+            size: 40,
+          ),
+        ));
   }
 }
--- skysync/lib/screens/resultpage.dart
+++ skysync/lib/screens/resultpage.dart
@@ -1,20 +1,60 @@
-// ignore_for_file: prefer_const_constructors, unnecessary_import, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, unnecessary_import, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, unused_import, unused_local_variable, prefer_typing_uninitialized_variables, non_constant_identifier_names, avoid_print
 
+import 'dart:convert';
 import 'dart:ui';
-
+import 'package:skysync/function/weatherstatus.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:skysync/screens/loading.dart';
 
 class ResultPage extends StatefulWidget {
-  const ResultPage({super.key});
+  const ResultPage({super.key, this.weatherdata});
 
+  final weatherdata;
   @override
   State<ResultPage> createState() => _ResultPageState();
 }
 
 class _ResultPageState extends State<ResultPage> {
+   WeatherModel weathermodel = WeatherModel();
+  int? tempreture;
+  int? feels;
+  double? max;
+  double? min;
+  int? humudity;
+  int? pressure;
+  int? visiblity;
+  double? speed;
+  String? cityname;
+  String? description;
+  String? weathericon;
+  late int cond;
+  @override
+  
+  void initState() {
+    super.initState();
+    Updateui(widget.weatherdata);
+  }
+
+  void Updateui(dynamic weatherdata) {
+    double temp = jsonDecode(weatherdata)['main']['temp'];
+    tempreture = temp.toInt();
+    feels = jsonDecode(weatherdata)['main']['feels_like'];
+    max = jsonDecode(weatherdata)['main']['temp_max'];
+    min = jsonDecode(weatherdata)['main']['temp_min'];
+    humudity = jsonDecode(weatherdata)['main']['humidity'];
+    visiblity = jsonDecode(weatherdata)['visibility'];
+    speed = jsonDecode(weatherdata)['wind']['speed'];
+    cityname = jsonDecode(weatherdata)['name'];
+    description = jsonDecode(weatherdata)['weather'][0]['description'];
+    pressure = jsonDecode(weatherdata)['main']['pressure'];
+    cond = jsonDecode(weatherdata)['weather'][0]['id'];
+   
+    weathericon=weathermodel.getWeatherIcon(cond);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -27,21 +67,16 @@ class _ResultPageState extends State<ResultPage> {
           title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              
-              Icon(Icons.location_on), 
+              Icon(Icons.location_on),
               Text(
-              "cityname",
-              style: TextStyle(
-                  backgroundColor: Color.fromARGB(0, 50, 50, 50),
-                  color: Color.fromARGB(255, 255, 255, 255)),
-                  
-            ),
-            
-            SizedBox(width:10),
-            
+                '$cityname',
+                style: TextStyle(
+                    backgroundColor: Color.fromARGB(0, 50, 50, 50),
+                    color: Color.fromARGB(255, 255, 255, 255)),
+              ),
+              SizedBox(width: 10),
             ],
           ),
-        
         ),
         body: Container(
           decoration: BoxDecoration(
@@ -49,197 +84,242 @@ class _ResultPageState extends State<ResultPage> {
             image: AssetImage('images/download.jpeg'),
             fit: BoxFit.cover,
           )),
-          child: Column( 
-            
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [ 
-            Text("32" , style: TextStyle(fontSize: 30 , color: const Color.fromARGB(255, 255, 255, 255)),), 
-            Text("shower rain" , style: TextStyle(fontSize: 30 , color: Color.fromARGB(255, 255, 255, 255))), 
-            Row(children: [ 
-              Expanded(
-                child: Container( 
-                  margin: EdgeInsets.only(left:10),
-                  
-                  height: 200,
-                  width: 200,
-                
-                  decoration: BoxDecoration(color:Colors.black.withOpacity(0.5) ,borderRadius: BorderRadius.circular(10)),
-                  child:Column(
-                    
-                    children: [
-                    Padding(padding: EdgeInsets.all(10)),
-                    Text("Tempreture",  style: TextStyle(color:Colors.white , fontSize: 20),), 
-                    SizedBox(
-                      height: 10,
-                    ),
-                   
-                    
-                    Text("34",style: TextStyle(color:Colors.white)),
-                    SizedBox(
+            children: [
+              Text(
+                '$tempreture°',
+                style: TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 255, 255, 255)),
+              ),
+              Text("$description",
+                  style: TextStyle(
+                      fontSize: 30, color: Color.fromARGB(255, 255, 255, 255))),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                        margin: EdgeInsets.only(left: 10),
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          children: [
+                            Padding(padding: EdgeInsets.all(10)),
+                            Text(
+                              "Tempreture",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                            SizedBox(
                               height: 10,
                             ),
-                    Text("Feels like" , style: TextStyle(color: Colors.white, fontSize: 20)),
-                    SizedBox(
+                            Text("$tempreture°",
+                                style: TextStyle(color: Colors.white)),
+                            SizedBox(
                               height: 10,
                             ),
-                    Text("43"  , style: TextStyle(color: Colors.white)),
-                  ],)
-                 ),
-              ), 
-               Expanded(
-                 child: Container(
+                            Text("",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20)),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(weathericon!,
+                                style: TextStyle(color: Colors.white, fontSize: 50)),
+                          ],
+                        )),
+                  ),
+                  Expanded(
+                    child: Container(
                         decoration: BoxDecoration(
                           color: Colors.black.withOpacity(0.5),
                           borderRadius: BorderRadius.circular(10),
                         ),
                         margin: EdgeInsets.all(20),
                         height: 200,
-                        width:300,
-                        child: Column(children: [ 
-                          Padding(padding: EdgeInsets.all(10)), 
-                          Text("On the day " , style:TextStyle(color: Colors.white, fontSize: 20 )), 
-                          SizedBox(height: 20,),
-                          Row(children: [
-
+                        width: 300,
+                        child: Column(
+                          children: [
                             Padding(padding: EdgeInsets.all(10)),
-
-                            Icon(Icons.arrow_upward , size: 30, color: Colors.white,), 
+                            Text("On the day",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20)),
                             SizedBox(
-                              width: 20,
-                            ) ,
-                            Text("max",
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 20)) ,
-                                        SizedBox(width: 10,),
-                                         Text("37",
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 15))],),
-                                        SizedBox(height:30),
-                          Row(
+                              height: 20,
+                            ),
+                            Row(
                               children: [
                                 Padding(padding: EdgeInsets.all(10)),
-                                Icon(Icons.arrow_downward, size: 30, color: Colors.white,),
+                                Icon(
+                                  Icons.arrow_upward,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
                                 SizedBox(
                                   width: 20,
                                 ),
-                                Text("min" , style: TextStyle(color:Colors.white , fontSize: 20),),
-                                SizedBox( width: 10,), 
-                                Text("33",
+                                Text("max",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20)),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text("$max",
                                     style: TextStyle(
                                         color: Colors.white, fontSize: 15))
                               ],
                             ),
-                        ],)),
-               )
-            ],), 
-            Container(
-              decoration: BoxDecoration(  
-                color:Colors.black.withOpacity(0.5) , 
-                borderRadius: BorderRadius.circular(10),
-                
+                            SizedBox(height: 30),
+                            Row(
+                              children: [
+                                Padding(padding: EdgeInsets.all(10)),
+                                Icon(
+                                  Icons.arrow_downward,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Text(
+                                  "min",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text("$min",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15))
+                              ],
+                            ),
+                          ],
+                        )),
+                  )
+                ],
               ),
-              margin: EdgeInsets.all(10),
-              height: 200,
-              width: 200,
-              child:Column(children: [
-                Row(children: [ 
-                  Padding(padding: EdgeInsets.all(20)), 
-                    Text("Humudity" ,
-                            style: TextStyle(color: Colors.white, fontSize: 20), ), 
-                            SizedBox(width:200) ,
-                        Text(                
-                            "11%",
+              Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  margin: EdgeInsets.all(10),
+                  height: 200,
+                  width: 200,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(padding: EdgeInsets.all(20)),
+                          Text(
+                            "Humudity",
                             style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
-                             ]
-                            
-                            
-                            ,), 
-                            Row(
-                        children: [
-                           Padding(padding: EdgeInsets.all(20)), 
+                          SizedBox(width: 200),
                           Text(
-                            "Presurre",
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                            
-                          ), SizedBox(width: 150),
-                          Text(
-                            "1006 mBar",
+                            "$humudity%",
                             style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                         ],
-                      ), 
+                      ),
                       Row(
                         children: [
-                           Padding(padding: EdgeInsets.all(20)), 
+                          Padding(padding: EdgeInsets.all(20)),
+                          Text(
+                            "Pressure",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          SizedBox(width: 150),
+                          Text(
+                            "$pressure mBar",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Padding(padding: EdgeInsets.all(20)),
                           Text(
                             "Visiblity",
                             style: TextStyle(color: Colors.white, fontSize: 20),
-                          ), SizedBox(width: 200),
+                          ),
+                          SizedBox(width: 170),
                           Text(
-                            "6 km",
+                            "$visiblity km",
                             style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                         ],
-                      ) , 
+                      ),
                       Row(
                         children: [
-                            Padding(padding: EdgeInsets.all(20)), 
+                          Padding(padding: EdgeInsets.all(20)),
                           Text(
                             "Wind speed",
                             style: TextStyle(color: Colors.white, fontSize: 20),
-                          ), SizedBox(width: 130),
+                          ),
+                          SizedBox(width: 130),
                           Text(
-                            "16.67 km/hr",
+                            "$speed km/hr",
                             style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                         ],
-                      ), 
-                      
-              ],)
-            ), 
-            Container(
-              decoration: BoxDecoration( 
-                color:Colors.black.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(20), 
+                      ),
+                    ],
+                  )),
+              Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(20),
+                  
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: 40),
+                    Text(
+                      "Sunrise:",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    SizedBox(width: 17),
+                    Text(
+                      "3:35",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    SizedBox(width: 15),
+                    // Icon(
+                    //   Icons.icon,
+                    //   size: 50,
+                    //   color: Colors.yellow,
+                    // ), 
+                    Text(
+                      weathericon! , style: TextStyle(fontSize: 20),
+                    ),
 
-              ),
-              child: Row(children: [ 
-                SizedBox(width:30), 
-                Text( "Sunrise:" ,
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      "Sunset",
                       style: TextStyle(color: Colors.white, fontSize: 20),
                     ),
-                   SizedBox(width:10),
-                   Text("3:35",
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    ) , 
-                    SizedBox(
-                      width:10
-                    ),
-                    Icon(Icons.sunny , size: 50 , color: Colors.yellow,), 
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text("Sunset",
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    ) , 
-                    SizedBox(
-                      width:10
-                    ),
-                   Text(
+                    SizedBox(width: 15),
+                    Text(
                       "7:75",
                       style: TextStyle(color: Colors.white, fontSize: 20),
                     ),
                     SizedBox(width: 10),
-              ],),
-            )
-
-            
-          ],
-            
-            ), 
-           
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       )),
     );
